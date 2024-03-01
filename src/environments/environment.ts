810c7f1b33e10bef4e19fe10fb58d0d92a300f55
Kesare_Cooray <121740972+Sahasra-Kesara@users.noreply.github.com>
2024-03-01 23:21:04 +0530

    Update environment.ts (Removed the API Key)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
     XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
     XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
     XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
-    XRapidAPIKeyHeaderValue: 'a522597718mshcee2b1754fa8cb3p133f1cjsn780501d755a1'
+    XRapidAPIKeyHeaderValue: ''
 };
